1e790c12dd18de53657b1399904b301b520092a6
huyentrang <33859157+17211TT2518@users.noreply.github.com>
2019-09-18 00:09:47 +0700

    finish_1455

--- 1455/1455-content.php
+++ 1455/1455-content.php
@@ -1,10 +1,7 @@
 <div class="type-1455">
-    <div class="container">
-        <div class="vincent_inner_text">
-            <img class="image-size" src="<?php echo $url_path ?>/images/2.png">
-            <div>
-                <h2 class="top">MEET OUR AWESOME TEAM</h2>
-            </div>
+    <div class="row">
+        <div class="abc">
+            <p>meet our awesome team</p>
         </div>
     </div>
 </div>
