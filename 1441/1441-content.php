5892dcf190345bc3108cb7f61995d4999d10f52d
huyentrang <33859157+17211TT2518@users.noreply.github.com>
2019-09-17 23:02:01 +0700

    finish_1441

--- 1441/1441-content.php
+++ 1441/1441-content.php
@@ -3,17 +3,17 @@
         <div class="row">
             <div class="content">
                 <div class="col-md-4 text-center">
-                    <img  src="<?php echo $url_path ?>/images/3.png"/>
+                    <img  src="<?php echo $url_path ?>/images/3.png" alt="responsive"/>
                     <h2>QUALITY FOODS</h2>
                     <p>Sit amet, consectetur adipiscing elit quisque eget maximus velit, non eleifend libero curabitur dapibus mauris sed leo cursus aliquetcras suscipit.</p>
                 </div>
                 <div class="col-md-4 text-center">
-                    <img  src="<?php echo $url_path ?>/images/4.png"/>
+                    <img  src="<?php echo $url_path ?>/images/4.png" alt="responsive"/>
                     <h2>FASTEST DELIVER</h2>
                     <p>Sit amet, consectetur adipiscing elit quisque eget maximus velit, non eleifend libero curabitur dapibus mauris sed leo cursus aliquetcras suscipit.</p>
                 </div>
                 <div class="col-md-4 text-center">
-                    <img  src="<?php echo $url_path ?>/images/5.png"/>
+                    <img  src="<?php echo $url_path ?>/images/5.png" alt="responsive"/>
                     <h2>ORIGINAL RECIPES</h2>
                     <p>Sit amet, consectetur adipiscing elit quisque eget maximus velit, non eleifend libero curabitur dapibus mauris sed leo cursus aliquetcras suscipit.</p>
                 </div>
