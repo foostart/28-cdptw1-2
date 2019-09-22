5a517b2ac995beccda54f735c26f800afb512121
huyentrang <33859157+17211TT2518@users.noreply.github.com>
2019-09-22 14:06:19 +0700

    Huyentrang_22/9/2019
    - Làm lại module 1455.

--- 1455/1455-content.php
+++ 1455/1455-content.php
@@ -1,7 +1,17 @@
 <div class="type-1455">
-    <div class="row">
-        <div class="abc">
-            <p>meet our awesome team</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 col-sm-offset-2">
+                <div class="header">
+                    <h1>Banner Templates</h1>
+                    <p>Start from any banner template and create amazing ads that actually convert.
+                        High-Quality, free banner design templates all available in our editor</p>
+                </div>
+                <div class="inputWithIcon">
+                        <input type="search" placeholder="Search design templates.... ">
+                        <i class="fa fa-search" aria-hidden="true"></i>              
+                </div>
+            </div>
         </div>
     </div>
 </div>
