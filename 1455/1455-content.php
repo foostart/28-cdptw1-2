0a4a67b99c2c888679a6c210f979336b80bc3e66
17211TT2518 <huyentrang2518.tdc.edu@gmail.com>
2019-09-30 09:41:13 +0700

    30/9
    1455

--- 1455/1455-content.php
+++ 1455/1455-content.php
@@ -1,19 +1,19 @@
 <div class="type-1455">
-    
-<section class="search-banner text-white py-3 form-arka-plan" id="search-banner">
-<div id="menu">
-        <ul>
-          <li><a href="#">HOME</a></li>
-          <li><a href="#">SHORTCODE</a></li>
-          <li><a href="#">CONTACT</a></li>
-          <li><a href="#">ABOUTS</a></li>
-          <li><a href="#">NOTIFICATION</a></li>
-        </ul>
+
+    <section class="search-banner text-white py-3 form-arka-plan" id="search-banner">
+        <div id="menu">
+            <ul>
+              <li><a href="#">HOME</a></li>
+              <li><a href="#">SHORTCODE</a></li>
+              <li><a href="#">CONTACT</a></li>
+              <li><a href="#">ABOUTS</a></li>
+              <li><a href="#">NOTIFICATION</a></li>
+          </ul>
       </div>
-    <div class="container py-5 my-5">
+      <div class="container py-5 my-5">
         <div class="row text-center pb-4">
             <div class="col-md-12">
-                <h2 class="text-white siyah-cerceve">Kiralık Otobüs Ara, Firmalar, Araçlar</h2>
+                <h2 class="text-white siyah-cerceve">Template Form Search</h2>
             </div>
         </div>
         <div class="row">
@@ -24,42 +24,43 @@
                             <div class="col-md-4">
                                 <div class="form-group ">
                                     <select id="iller" class="form-control" >
-                                        <option selected>Tüm İller</option>
-                                        <option>İstanbul</option>
-                                        <option>Ankara</option>
+                                        <option selected>District 1</option>
+                                        <option>District 2</option>
+                                        <option>District 3</option>
+                                        <option>District 3</option>
                                     </select>
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="form-group ">
                                     <select id="ilceler" class="form-control" >
-                                        <option selected>Tüm İlçeler</option>
-                                        <option>BMW</option>
-                                        <option>Audi</option>
-                                        <option>Maruti</option>
-                                        <option>Tesla</option>
+                                        <option selected>Ho Chi Minh City</option>
+                                        <option>Da Nang City</option>
+                                        <option>...</option>
+                                        <option>...</option>
                                     </select>
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="form-group ">
                                     <select id="arac-turu" class="form-control" >
-                                        <option selected>Tüm Araç Türleri</option>
-                                        <option>Otobüs</option>
-                                        <option>Minibüs</option>
-                                        <option>Karavan</option>
-                                        <option>Vito</option>
-                                        <option>Limuzin</option>
+                                        <option selected>20 ages</option>
+                                        <option>21 ages</option>
+                                        <option>22 ages</option>
+                                        <option>23 ages</option>
+                                        <option>24 ages</option>
+                                        <option>25 ages</option>
 
                                     </select>
                                 </div>
                             </div>
                         </div>
-                       <br><br>
+                        <br><br>
                         <div class="row">
                             <div class="col-md-9">
                                 <div class="form-group ">
-                                    <input type="text" class="form-control" placeholder="Firma, şahis, araç ismiyle arayabilirsin">
+                                    <input type="text" class="form-control" placeholder="
+                                    Please enter your name to search..........">
                                 </div>
                             </div>
                             <div class="col-md-3">
