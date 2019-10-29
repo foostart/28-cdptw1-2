5e90e906aaf6fb0608f530a3d32aaac9d202f4fd
huyentrang <huyentrang2518.tdc.edu@gmail.com>
2019-10-29 08:21:25 +0700

    29/10

--- 1441/1441.php
+++ 1441/1441.php
@@ -1,14 +1,18 @@
 <?php
-$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$url_host = 'http://'.$_SERVER['HTTP_HOST'];
 $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
 $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
-
 preg_match_all($pattern_uri, __DIR__, $matches);
 $url_path = $url_host . $matches[1][0];
 $url_path = str_replace('\\', '/', $url_path);
-
-
+if (!class_exists('lessc')) {
+$dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
+require_once($dir_block.'/libs/lessc.inc.php');
+}
+$less = new lessc;
+$less->compileFile('less/1441.less', 'css/1441.css');
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
