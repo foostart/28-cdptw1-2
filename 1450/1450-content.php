cb3633f39054f6e9dc5c8fb9204045b6b1efcec2
huyentrang <huyentrang2518.tdc.edu@gmail.com>
2019-10-31 09:01:57 +0700

    31/10

--- 1450/1450-content.php
+++ 1450/1450-content.php
@@ -1,59 +1,44 @@
 <div class="type-1450">
-    <div class="container collas">
-        <div class="row custom">
-            <div class="col-md-3 col-ms-6">
-            <a href="#" id="des">Description</a>
-            </div>
-            <div class="col-md-4 col-ms-6">
-            <a href="#" id="ship">Additional information</a>
-            </div>
-            <div class="col-md-3 col-ms-6">
-            <a href="#" id="rev">Reviews(0)</a>
-            </div>
-           
-        </div>
-       
-      
-        <!-- description -->
 
-        <div id="footwear" class="style-all">
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis adipisci reiciendis nemo a commodi iure. Iste, laborum. Perferendis eligendi repellendus quos tempora? Delectus facere voluptate reiciendis qui veniam vitae perferendis!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, quaerat modi omnis voluptates corrupti et velit quia odio consectetur quos autem reiciendis eligendi provident, saepe nostrum dolores blanditiis optio magnam!
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores quisquam nam inventore excepturi commodi nihil, praesentium cupiditate consectetur quae consequatur labore sed voluptas architecto reprehenderit distinctio, nesciunt aspernatur a similique.
-            </p>
-        </div>
+    <div class="container">
+        <ul class="nav nav-tabs">
+            <li class="active"><a data-toggle="tab" href="#home">Description</a></li>
+            <li><a data-toggle="tab" href="#menu1">Additional information</a></li>
+            <li><a data-toggle="tab" href="#menu2">Review(0)</a></li>
+        </ul>
 
-        <!-- reviews -->
-
-        <div id="review" class="style-rev">
-           <h2>No review</h2>
-        </div>
-
-        <!-- Addition -->
-        <div id="deliver" class="style-ship">
-            <div class="row">
-                <div class="col-md-4 one">
-                <h2>Weight</h2>
-               <p>1kg</p>
-                <h2>Dimensions</h2>
-              <p>1 x 1 x 1 cm</p>
-                </div>
-                <div class="col-md-4 two">
-                <h2>Weight</h2>
-               <p>1kg</p>
-                <h2>Dimensions</h2>
-              <p>1 x 1 x 1 cm</p>
-                </div>
-                <div class="col-md-4 two">
-                <h2>Weight</h2>
-               <p>1kg</p>
-                <h2>Dimensions</h2>
-              <p>1 x 1 x 1 cm</p>
-                </div>
+        <div class="tab-content">
+            <div id="home" class="tab-pane fade in active">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nulla quis laudantium libero unde laborum earum eum suscipit, voluptas tempora illum corrupti, nemo doloremque nesciunt tempore explicabo eveniet eaque nobis.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quos fugiat distinctio facere asperiores neque, autem quasi praesentium itaque veritatis enim repellat, vero amet consequatur nobis harum magni illo? Animi!
+                </p>
             </div>
-            
+            <div id="menu1" class="tab-pane fade">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <h2>Weight</h2>
+                            <p>1kg</p>
+                            <h2>Dimensions</h2>
+                            <p>1 x 1 x 1 cm</p>
+                        </div>
+                        <div class="col-md-4">
+                            <h2>Weight</h2>
+                            <p>1kg</p>
+                            <h2>Dimensions</h2>
+                            <p>1 x 1 x 1 cm</p>
+                        </div>
+                        <div class="col-md-4">
+                            <h2>Weight</h2>
+                            <p>1kg</p>
+                            <h2>Dimensions</h2>
+                            <p>1 x 1 x 1 cm</p>
+                        </div>
+                    </div>
+            </div>
+            <div id="menu2" class="tab-pane fade">
+                <p>No review</p>
+            </div>
         </div>
     </div>
-</div>       
-
+</div>
--- 1450/js/1450.js
+++ 1450/js/1450.js
@@ -1,26 +1,7 @@
 $(document).ready(function() {
-  $("#des").click(function() {
-      $("#footwear").css("visibility", "visible");
-      $("#review").css("visibility", "hidden");
-      $("#deliver").css("visibility", "hidden");
+  $(".btn-pref .btn").click(function () {
+      $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
+      // $(".tab").addClass("active"); // instead of this do the below 
+      $(this).removeClass("btn-default").addClass("btn-primary");   
   });
-  $("#rev").click(function() {
-      $("#review").css("visibility", "visible");
-      $("#footwear").css("visibility", "hidden");
-      $("#deliver").css("visibility", "hidden");
-      $('.save').click(function() {
-          
-          if($('#check').is(':checked')){
-            $('#check').prop("checked", false);
-          }else{
-            $('#check').prop("checked", true);
-          }
-      });
   });
-  $("#ship").click(function() {
-      $("#deliver").css("visibility", "visible");
-      $("#footwear").css("visibility", "hidden");
-      $("#review").css("visibility", "hidden");
-  });
-
-});
